cc276e832f2164b6400c37b2bcb33a343d796d94
Phil Cooper-King <phil@cooperking.net>
2019-05-22 07:38:19 +0100

    includes fix

--- premake5.lua
+++ premake5.lua
@@ -330,22 +330,19 @@ dep_search(root, curr_proj, projects)
 
                 break;
             end
-
-            local dep_str = "BUILD_HAS_" .. dep_name .. "="
-            dep_str = string.upper()
-
-            local dep_inc_str = "BUILD_" .. dep_name .. "_INCLUDED="
-            dep_inc_str = string.upper()
-            
-            if has_dep then 
-                dep_str = dep_str .. "1"
-                table.insert(root.defines, dep_inc_str)
-            else
-                dep_str = dep_str .. "0"
-            end
-
-            table.insert(root.defines, dep_str)
         end
+
+        local dep_str = "BUILD_HAS_" .. string.upper(dep_name) .. "="
+        local dep_inc_str = "BUILD_" .. string.upper(dep_name) .. "_INCLUDED"
+        
+        if has_dep then 
+            dep_str = dep_str .. "1"
+            table.insert(root.defines, dep_inc_str)
+        else
+            dep_str = dep_str .. "0"
+        end
+
+        table.insert(root.defines, dep_str)
          
     end
 
